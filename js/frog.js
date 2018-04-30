808a4bde22009e5e3aff4397cb5035dda3c2ecc9
Chen Wei <s3499257@student.rmit.edu.au>
2018-04-30 14:52:17 +1000

    torso 2nd try

--- js/frog.js
+++ js/frog.js
@@ -16,12 +16,12 @@ var r36 = (36 * pi) / 180
 var r72 = (72 * pi) / 180
 //vertices of torso
 torso.vertices.push(new THREE.Vector3(Math.cos(r36),0,Math.sin(r36)));
-torso.vertices.push(new THREE.Vector3(- Math.cos(r72),0,Math.sin(r72)));
+torso.vertices.push(new THREE.Vector3(-Math.cos(r72),0,Math.sin(r72)));
 torso.vertices.push(new THREE.Vector3(-1,0,0));
-torso.vertices.push(new THREE.Vector3(- Math.cos(r72),0,- Math.sin(r72)));
-torso.vertices.push(new THREE.Vector3(- Math.cos(Math.cos(r36),0,- Math.sin(r36))));
-torso.vertices.push(new THREE.Vector3(- Math.cos(0,0.5,0)));
-torso.vertices.push(new THREE.Vector3(- Math.cos(0,-0.5,0)));
+torso.vertices.push(new THREE.Vector3(-Math.cos(r72),0,-Math.sin(r72)));
+torso.vertices.push(new THREE.Vector3(Math.cos(r36),0,-Math.sin(r36)));
+torso.vertices.push(new THREE.Vector3(0,0.5,0));
+torso.vertices.push(new THREE.Vector3(0,-0.5,0));
 //faces of torso
 torso.faces.push(new THREE.Face3(1,0,5));
 torso.faces.push(new THREE.Face3(0,4,5));
@@ -38,6 +38,8 @@ var material = new THREE.MeshBasicMaterial({color:0x00ff00});
 var object = new THREE.Mesh(torso, material);
 
 scene.add(object)
+
+
 
 
 camera.position.z = 5;
