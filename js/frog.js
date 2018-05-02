0955af2944cafef66755fe6872128e40d41dd746
Tengku Ummil Syazwina Zainab <s3575233@student.rmit.edu.au>
2018-05-03 02:35:58 +1000

    torso and head

--- js/frog.js
+++ js/frog.js
@@ -1,4 +1,3 @@
-
 //init the scene
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
@@ -16,15 +15,10 @@ var r36 = (36 * pi) / 180;
 var r72 = (72 * pi) / 180;
 //vertices of torso
 torso.vertices.push(new THREE.Vector3(Math.cos(r36),0,Math.sin(r36)));
-torso.vertices.push(new THREE.Vector3(-Math.cos(r72),0,Math.sin(r72)));
+torso.vertices.push(new THREE.Vector3(- Math.cos(r72),0,Math.sin(r72)));
 torso.vertices.push(new THREE.Vector3(-1,0,0));
-<<<<<<< HEAD
 torso.vertices.push(new THREE.Vector3(- Math.cos(r72),0,- Math.sin(r72)));
 torso.vertices.push(new THREE.Vector3(- Math.cos(Math.cos(r36),0,- Math.sin(r36))));
-=======
-torso.vertices.push(new THREE.Vector3(-Math.cos(r72),0,-Math.sin(r72)));
-torso.vertices.push(new THREE.Vector3(Math.cos(r36),0,-Math.sin(r36)));
->>>>>>> 808a4bde22009e5e3aff4397cb5035dda3c2ecc9
 torso.vertices.push(new THREE.Vector3(0,0.5,0));
 torso.vertices.push(new THREE.Vector3(0,-0.5,0));
 //faces of torso
@@ -59,7 +53,6 @@ head.faces.push(new THREE.Face3(3,5,2));
 head.faces.push(new THREE.Face3(0,1,5));
 
 
-
 var material = new THREE.MeshBasicMaterial({color:0x00ff00});
 var object = new THREE.Mesh(torso, material);
 var object2 = new THREE.Mesh(head, material);
@@ -73,8 +66,6 @@ var axes = new THREE.AxesHelper(5);
 var axes2 = new THREE.AxesHelper(5);
 object.add(axes);
 object2.add(axes2);
-
-
 
 
 camera.position.z = 5;
